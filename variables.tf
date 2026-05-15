f93cdfdb695a9a1f245db3993bcd60c7fd1314a9
SunnovaraTechnologies <ranjith.rajkumar.st@gmail.com>
2026-05-15 19:27:29 +0530

    Add/Update variables.tf via Terraform Agent

--- variables.tf
+++ variables.tf
@@ -1,10 +1,17 @@
+// variables.tf
 variable "aws_region" {
-  description = "The AWS region to deploy resources in."
+  description = "AWS region to deploy resources in."
   type        = string
   default     = "us-east-1"
 }
 
 variable "bucket_name" {
-  description = "The name of the S3 bucket."
+  description = "Name of the S3 bucket."
   type        = string
+}
+
+variable "kms_key_alias" {
+  description = "Alias for the KMS key."
+  type        = string
+  default     = "alias/s3-bucket-key"
 }
