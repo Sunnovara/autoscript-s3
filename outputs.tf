ea2d92f68a952bc2c5e95c9dd6df1fee14d65284
SunnovaraTechnologies <ranjith.rajkumar.st@gmail.com>
2026-05-15 19:00:44 +0530

    Add/Update outputs.tf via Terraform Agent

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "bucket_arn" {
   description = "The ARN of the S3 bucket."
   value       = aws_s3_bucket.this.arn
 }
+
+output "kms_key_arn" {
+  description = "The ARN of the KMS key used for S3 bucket encryption."
+  value       = aws_kms_key.s3.arn
+}
