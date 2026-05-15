1f4c891fa6e7087a7e1fd07a0bdd65c7bc65625d
SunnovaraTechnologies <ranjith.rajkumar.st@gmail.com>
2026-05-15 19:27:44 +0530

    Add/Update modules/s3_bucket/variables.tf via Terraform Agent

---
+++ modules/s3_bucket/variables.tf
@@ -0,0 +1,10 @@
+// modules/s3_bucket/variables.tf
+variable "bucket_name" {
+  description = "Name of the S3 bucket."
+  type        = string
+}
+
+variable "kms_key_arn" {
+  description = "ARN of the KMS key for encryption."
+  type        = string
+}
