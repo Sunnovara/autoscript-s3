e0977f6651bdd14265b5fc66ed34f28a62436d21
SunnovaraTechnologies <ranjith.rajkumar.st@gmail.com>
2026-05-15 19:27:33 +0530

    Add/Update main.tf via Terraform Agent

--- main.tf
+++ main.tf
@@ -1,26 +1,11 @@
-resource "aws_kms_key" "s3" {
-  description             = "KMS key for S3 bucket server-side encryption"
-  deletion_window_in_days = 7
-  enable_key_rotation     = true
+// main.tf
+module "kms_key" {
+  source        = "./modules/kms_key"
+  kms_key_alias = var.kms_key_alias
 }
 
-resource "aws_s3_bucket" "this" {
-  bucket = var.bucket_name
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = "aws:kms"
-        kms_master_key_id = aws_kms_key.s3.arn
-      }
-    }
-  }
-}
-
-resource "aws_s3_bucket_versioning" "this" {
-  bucket = aws_s3_bucket.this.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
+module "s3_bucket" {
+  source         = "./modules/s3_bucket"
+  bucket_name    = var.bucket_name
+  kms_key_arn    = module.kms_key.kms_key_arn
 }
