aa2c9dee37aef08a24dda6ceb3c208afa166f963
SunnovaraTechnologies <ranjith.rajkumar.st@gmail.com>
2026-05-31 16:23:41 +0530

    Add/Update outputs.tf via Terraform Agent

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,4 @@
-// outputs.tf
 output "s3_bucket_id" {
-  description = "ID of the S3 bucket."
-  value       = module.s3_bucket.bucket_id
-}
-
-output "kms_key_arn" {
-  description = "ARN of the KMS key."
-  value       = module.kms_key.kms_key_arn
+  description = "The name of the S3 bucket."
+  value       = aws_s3_bucket.this.id
 }
